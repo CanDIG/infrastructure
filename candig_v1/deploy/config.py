8dce1b419a5fc793e2979d67ee5d6708f15440f7
daisie_local <daisieh@gmail.com>
2021-02-16 17:49:04 -0800

    edits

--- candig_v1/deploy/config.py
+++ candig_v1/deploy/config.py
@@ -1,1 +1,20 @@
+# Production config
 DATA_SOURCE = "/home/vagrant/candig-example-data/registry.db"
+# REQUEST_VALIDATION = True
+# ACCESS_LIST = '/srv/candig/access/access_list.txt'
+# DP_EPSILON = None
+# 
+# # Tyk settings
+# TYK_ENABLED = True
+# TYK_SERVER = 'https://candig.uhnresearch.ca'
+# TYK_LISTEN_PATH = ''
+# 
+# # Keycloak settings with redirection through tyk
+# KC_REALM = 'CanDIG'
+# KC_SERVER = 'https://candigauth.uhnresearch.ca'
+# KC_SCOPE = 'openid+email'
+# KC_RTYPE = 'code'
+# KC_CLIENT_ID = 'uhn_candig'
+# KC_RMODE = 'form_post'
+# KC_REDIRECT = TYK_SERVER+TYK_LISTEN_PATH+'/login_oidc'
+# KC_LOGIN_REDIRECT = '/auth/realms/{0}/protocol/openid-connect/auth?scope={1}&response_type={2}&client_id={3}&response_mode={4}&redirect_uri={5}'.format(KC_REALM, KC_SCOPE, KC_RTYPE, KC_CLIENT_ID, KC_RMODE, KC_REDIRECT)
